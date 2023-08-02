9017a105959e80089f8ab90d1b8ffaac793acf53
couriourc <3234853521@qq.com>
2023-08-02 22:36:02 +0800

    migrate project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,47 @@
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
+import {Analytics} from "@vercel/analytics/react";
 import cx from "classnames";
-import { sfPro, inter } from "./fonts";
+import {inter, sfPro} from "./fonts";
 import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
-import { Suspense } from "react";
+import {Suspense} from "react";
+import {Metadata} from "next";
 
-export const metadata = {
-  title: "Precedent - Building blocks for your Next.js project",
-  description:
-    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  twitter: {
-    card: "summary_large_image",
-    title: "Precedent - Building blocks for your Next.js project",
+export const metadata: Metadata = {
+    title: "Muser",
     description:
-      "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-    creator: "@steventey",
-  },
-  metadataBase: new URL("https://precedent.dev"),
-  themeColor: "#FFF",
+        "",
+    icons: ["/logo.png"],
+    twitter: {
+        card: "summary_large_image",
+        title: "Precedent - Building blocks for your Next.js project",
+        description:
+            "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
+        creator: "@couriourc",
+    },
+    metadataBase: new URL("https://muser.vercel.app"),
+    themeColor: "#FFF",
 };
 
 export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                             children,
+                                         }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+    return (
+        <html lang="en">
+        <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100"/>
         <Suspense fallback="...">
-          {/* @ts-expect-error Server Component */}
-          <Nav />
+            {/* @ts-expect-error Server Component */}
+            <Nav/>
         </Suspense>
         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
-          {children}
+            {children}
         </main>
-        <Footer />
-        <Analytics />
-      </body>
-    </html>
-  );
+        <Footer/>
+        <Analytics/>
+        </body>
+        </html>
+    );
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,11 +11,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: "https://precedent.dev",
+      url: "https://muser.vercel.app",
       lastModified: new Date(),
     },
     ...users.map((user) => ({
-      url: `https://precedent.dev/${user.id}`,
+      url: `https://muser.vercel.app/${user.id}`,
       lastModified: new Date(),
     })),
   ];
